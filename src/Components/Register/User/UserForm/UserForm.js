ce7ebf08e2b59607b4302163d84b777270835c64
Fefe Lemaire <fede.lemaire@gmail.com>
2020-07-29 11:20:56 -0300

    cambios de estilos en la caja de logueo, cambios de color de background de la caja, cambio de posicion de botones y espaciados

--- src/Components/Register/User/UserForm/UserForm.js
+++ src/Components/Register/User/UserForm/UserForm.js
@@ -34,11 +34,11 @@ const UserForm = ({ mainpage, language, score, gameIn, nextButton }) => (
                     aria-hidden="true"
                 >
                     <div
-                        className="modal-dialog modal-dialog-centered"
+                        className="modal-dialog modal-dialog-center"
                         role="document"
                     >
-                        <div className="modal-content">
-                            <div className="modal-header">
+                        <div className=" ">
+                            <div className="modal-header" style={{ width: '29em' }}>
                                 {/* <h5 className="modal-title" id="register">Welcome, keep playing!</h5> */}
                                 <button
                                     type="button"
@@ -52,24 +52,24 @@ const UserForm = ({ mainpage, language, score, gameIn, nextButton }) => (
                             {/* <div className="modal-body" /> */}
                             <div className="form-wrap">
                                 {/* <MyProvider> */}
-                                    <div className="tabs">
-                                        <Panel id="login">
-                                            {/* <h2 className="login-tab">{texts[language].login}</h2> */}
-                                            <h2 className="login-tab">{localStorage.language ? texts[localStorage.language].login : texts[lang1].login}</h2>
-                                        </Panel>
-                                        <Panel id="signup">
-                                            {/* <h2 className="signup-tab">{texts[language].signUp}</h2> */}
-                                            <h2 className="signup-tab">{localStorage.language ? texts[localStorage.language].signUp : texts[lang1].signUp}</h2>
-                                        </Panel>
-                                    </div>
+                                <div className="tabs">
+                                    <Panel id="login">
+                                        {/* <h2 className="login-tab">{texts[language].login}</h2> */}
+                                        <h2 className="login-tab">{localStorage.language ? texts[localStorage.language].login : texts[lang1].login}</h2>
+                                    </Panel>
+                                    <Panel id="signup">
+                                        {/* <h2 className="signup-tab">{texts[language].signUp}</h2> */}
+                                        <h2 className="signup-tab">{localStorage.language ? texts[localStorage.language].signUp : texts[lang1].signUp}</h2>
+                                    </Panel>
+                                </div>
 
-                                    <FormPanel isActive="login">
-                                        <Login language={lang1} pageIn="between-rounds" />
-                                    </FormPanel>
+                                <FormPanel isActive="login">
+                                    <Login language={lang1} pageIn="between-rounds" />
+                                </FormPanel>
 
-                                    <FormPanel isActive="signup">
-                                        <SignUp language={lang1} />
-                                    </FormPanel>
+                                <FormPanel isActive="signup">
+                                    <SignUp language={lang1} />
+                                </FormPanel>
                                 {/* </MyProvider> */}
                             </div>
                         </div>
@@ -117,24 +117,24 @@ const UserForm = ({ mainpage, language, score, gameIn, nextButton }) => (
                             <div className="modal-body" />
                             <div className="form-wrap">
                                 {/* <MyProvider> */}
-                                    <div className="tabs">
-                                        <Panel id="login">
-                                            {/* <h2 className="login-tab">{texts[language].login}</h2> */}
-                                            {localStorage.language ? texts[localStorage.language].login : texts[lang1].login}
-                                        </Panel>
-                                        <Panel id="signup">
-                                            {/* <h2 className="signup-tab">{texts[language].signUp}</h2> */}
-                                            {localStorage.language ? texts[localStorage.language].singUp : texts[lang1].singUp}
-                                        </Panel>
-                                    </div>
+                                <div className="tabs">
+                                    <Panel id="login">
+                                        {/* <h2 className="login-tab">{texts[language].login}</h2> */}
+                                        {localStorage.language ? texts[localStorage.language].login : texts[lang1].login}
+                                    </Panel>
+                                    <Panel id="signup">
+                                        {/* <h2 className="signup-tab">{texts[language].signUp}</h2> */}
+                                        {localStorage.language ? texts[localStorage.language].singUp : texts[lang1].singUp}
+                                    </Panel>
+                                </div>
 
-                                    <FormPanel isActive="login">
-                                        <Login pageIn="between-rounds" language={language} gameIn={gameIn} score={score} />
-                                    </FormPanel>
+                                <FormPanel isActive="login">
+                                    <Login pageIn="between-rounds" language={language} gameIn={gameIn} score={score} />
+                                </FormPanel>
 
-                                    <FormPanel isActive="signup">
-                                        <SignUp language={language} score={score} />
-                                    </FormPanel>
+                                <FormPanel isActive="signup">
+                                    <SignUp language={language} score={score} />
+                                </FormPanel>
                                 {/* </MyProvider> */}
                             </div>
                         </div>
