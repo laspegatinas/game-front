7b88d19788c17b878d7b73fe0eeec2c604737b82
Fefe Lemaire <fede.lemaire@gmail.com>
2020-07-31 10:34:37 -0300

    actualizacion del titulo de cada juego a 'juega con...'

--- src/Components/Rounds/Rounds.js
+++ src/Components/Rounds/Rounds.js
@@ -17,7 +17,7 @@ import PopUp from '../Rounds/PopUp';
 class Rounds extends React.Component {
 
     state = {
-        
+
         page: 'hideGame',
         button: 'btn-game',
         spotify: false,
@@ -25,17 +25,17 @@ class Rounds extends React.Component {
         instagram: false,
         accessToken: '',
         pop: false
-        
+
     }
 
 
     getToken = () => {
 
-         this.setState({
+        this.setState({
 
-             accessToken: Spotify.getaccessToken()
-          })
-        
+            accessToken: Spotify.getaccessToken()
+        })
+
     }
 
     startSpotify = () => {
@@ -77,16 +77,16 @@ class Rounds extends React.Component {
         });
     }
 
-  popStart = () => {
+    popStart = () => {
 
-      this.setState({
+        this.setState({
 
-        pop: true
-          
-      })
-  }
+            pop: true
 
-  
+        })
+    }
+
+
 
     render() {
 
@@ -98,143 +98,143 @@ class Rounds extends React.Component {
         const loginComp = (context) => {
 
             const { state: { username, email } } = context;
-    
+
             if (username || email) {
-               
+
                 return (
                     <Register buttonStyle={'button1'} buttonText={texts[language].startRound2Spotify}
-                     score={score} currentGame="spotify" language={language} />
+                        score={score} currentGame="spotify" language={language} />
                 );
             }
-    
+
             return <UserForm nextButton={'button1'} language={language} score={score} gameIn="spotify" />;
         };
 
         const loginComp2 = (context) => {
 
             const { state: { username, email } } = context;
-    
+
             if (username || email) {
-               
+
                 return (
-                    
+
                     <Register buttonStyle={'button1'} buttonText={texts[language].startRound2Instagram}
-                     score={score} currentGame="instagram" language={language} />
+                        score={score} currentGame="instagram" language={language} />
                 );
             }
-    
+
             return <UserForm nextButton={'button1'} language={language} score={score} gameIn="spotify" />;
         };
 
         return (
             <MyContext.Consumer>
-            {(context) => (
-                <>
-            <div>
-                
-                {/* displaying the appropriate introduction, depending on chosen game */}
-                <div className={page}>
-                    <div className={pop ? 'hideGame' : "title"}>
-                       <h1 >{texts[language].roundOneText}</h1>
-                    {spotify ?
-                        (
-                            <h4>{texts[language].youtubeInstructionsR1}</h4>
-                        )
-                        : instagram ?
-                        (
-                            <h4>{texts[language].youtubeInstructionsR1}</h4>
-                        )
-                        : youtube ?
-                        (
-                            <div>
-                                <h4>{texts[language].youtubeInstructionsR1}</h4>
-                                <h5>{texts[language].youtubeAddition}</h5> 
-                             </div>
-                        )
-                        : null
-                    }                                        
-                    </div>
+                {(context) => (
+                    <>
+                        <div>
 
-                    {/* displaying the appropriate button depending on user choise
+                            {/* displaying the appropriate introduction, depending on chosen game */}
+                            <div className={page}>
+                                <div className={pop ? 'hideGame' : "title"}>
+                                    <h1 >{spotify ? texts[language].spotifyPlayWithButton : youtube ? texts[language].youtubePlayWithButton : instagram ? texts[language].instagramPlayWithButton : null}</h1>
+                                    {spotify ?
+                                        (
+                                            <h4>{texts[language].youtubeInstructionsR1}</h4>
+                                        )
+                                        : instagram ?
+                                            (
+                                                <h4>{texts[language].youtubeInstructionsR1}</h4>
+                                            )
+                                            : youtube ?
+                                                (
+                                                    <div>
+                                                        <h4>{texts[language].youtubeInstructionsR1}</h4>
+                                                        <h5>{texts[language].youtubeAddition}</h5>
+                                                    </div>
+                                                )
+                                                : null
+                                    }
+                                </div>
+
+                                {/* displaying the appropriate button depending on user choise
                     {context.state.spotify_round_one ? context.state.spotify_round_one : texts[language].roundOneText} */}
 
-                    {/* button invokes a method that first shows popup and then with set timeout got to spotifyroundone 
+                                {/* button invokes a method that first shows popup and then with set timeout got to spotifyroundone 
                     clicked ? <SpotifyRoundone/>
                     <button className="button1" type="button">
                         {context.state.spotify_round_one || texts[language].roundOneText}</button>
                     
                     */}
-                  
-                    <div className={youtube || instagram ? 'hideGame' : 'title'}>
-                         <div className={pop ? 'hideGame' : ''}>'total pts: ' { context.state.spotify_round_one || ''}</div>
-                        <DelayLink  delay={5000} clickAction={this.popStart} to="spotifyroundone" >
-                          <PopUp language={language} todo={texts[language].popUp} instruct={texts[language].popUpSpotify}
-                           popButton={'button1'} popText={texts[language].roundOneText}/>
-                        </DelayLink> 
-                    </div>
-                    <div className={youtube || instagram || pop ? 'hideGame' : 'title'}>
-                    {context.state.spotify_round_two || ''}
-                        {loginComp(context)}</div>
-                    <Link className={youtube || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Spotify}</button></Link>
-                    
 
-                    <div className={spotify || instagram ? 'hideGame' : 'title'}>
-                        <div className={pop ? 'hideGame' : ''}> { context.state.youtube_round_one || ''}</div>
-                        <DelayLink  delay={5000} clickAction={this.popStart} to="youtuberoundone" >
-                          <PopUp language={language} todo={texts[language].popUp} instruct={texts[language].popUpYoutube}
-                           popButton={'button1'} popText={texts[language].roundOneText}/>
-                        </DelayLink> 
-                    </div>
-                    <Link className={spotify || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound2Youtube}</button></Link>
-                    <Link className={spotify || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Youtube}</button></Link>
-                
-                    <div className={spotify || youtube ? 'hideGame' : 'title'}>
-                        <div className={pop ? 'hideGame' : ''}> { context.state.instagram_round_one || ''}</div>
-                        <DelayLink  delay={5000} clickAction={this.popStart} to="instagramroundone" >
-                          <PopUp language={language} popButton={'button1'} todo={texts[language].popUp} instruct={texts[language].popUpInstagram}
-                           popText={texts[language].roundOneText}/>
-                        </DelayLink> 
-                    </div>
-                    {/* <Link className={spotify || youtube ? 'hideGame' : 'title'} to="instagramroundone"><button className="button1" type="button">{texts[language].startRound1}</button></Link> */}
-                    <div className={spotify || youtube || pop ? 'hideGame' : 'title'} >{context.state.instagram_round_two || ''}{loginComp2(context)}</div>
-                    <Link className={spotify || youtube || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Instagram}</button></Link>
-                </div>
-              
-                
-                <div className="home-play-buttons">
-                 
+                                <div className={youtube || instagram ? 'hideGame' : 'title'}>
+                                    <div className={pop ? 'hideGame' : ''}>'total pts: ' {context.state.spotify_round_one || ''}</div>
+                                    <DelayLink delay={5000} clickAction={this.popStart} to="spotifyroundone" >
+                                        <PopUp language={language} todo={texts[language].popUp} instruct={texts[language].popUpSpotify}
+                                            popButton={'button1'} popText={texts[language].roundOneBtn} />
+                                    </DelayLink>
+                                </div>
+                                <div className={youtube || instagram || pop ? 'hideGame' : 'title'}>
+                                    {context.state.spotify_round_two || ''}
+                                    {loginComp(context)}</div>
+                                <Link className={youtube || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Spotify}</button></Link>
 
-                    <div>
-                        {  this.state.accessToken ?
-                            <button type="button" className={this.props.spotifyButton} onClick={this.startSpotify}>
-                            <i class="fab fa-spotify"></i>
-                                {texts[language].spotifyPlayWithButton}
-                            </button>
-                            :
-                            <button className={this.props.tokenButton} onClick={this.getToken}>
-                              <i class="fab fa-spotify"></i>
-                             {texts[language].key}
-                            </button>
-                        }
-                    </div>
-                    
-                    <button type="button" className={this.props.youtubeButton} onClick={this.startYoutube}>
-                        <i class="fab fa-youtube"></i>
-                            {texts[language].youtubePlayWithButton}
-                    </button>
-                    <button type="button" className={this.props.instagramButton} onClick={this.startInsta}>
-                        <i class="fab fa-instagram"></i>
-                            {texts[language].instagramPlayWithButton}
-                    </button>
-                    <Link className={this.props.homeButton} to="/"><img className={"home-btn-image"} src={homebtn}/>
-                    {texts[language].home}</Link>
-                </div>
 
-            
-            </div>
-            </>
-            )}
-        </MyContext.Consumer>
+                                <div className={spotify || instagram ? 'hideGame' : 'title'}>
+                                    <div className={pop ? 'hideGame' : ''}> {context.state.youtube_round_one || ''}</div>
+                                    <DelayLink delay={5000} clickAction={this.popStart} to="youtuberoundone" >
+                                        <PopUp language={language} todo={texts[language].popUp} instruct={texts[language].popUpYoutube}
+                                            popButton={'button1'} popText={texts[language].roundOneBtn} />
+                                    </DelayLink>
+                                </div>
+                                <Link className={spotify || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound2Youtube}</button></Link>
+                                <Link className={spotify || instagram || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Youtube}</button></Link>
+
+                                <div className={spotify || youtube ? 'hideGame' : 'title'}>
+                                    <div className={pop ? 'hideGame' : ''}> {context.state.instagram_round_one || ''}</div>
+                                    <DelayLink delay={5000} clickAction={this.popStart} to="instagramroundone" >
+                                        <PopUp language={language} popButton={'button1'} todo={texts[language].popUp} instruct={texts[language].popUpInstagram}
+                                            popText={texts[language].roundOneBtn} />
+                                    </DelayLink>
+                                </div>
+                                {/* <Link className={spotify || youtube ? 'hideGame' : 'title'} to="instagramroundone"><button className="button1" type="button">{texts[language].startRound1}</button></Link> */}
+                                <div className={spotify || youtube || pop ? 'hideGame' : 'title'} >{context.state.instagram_round_two || ''}{loginComp2(context)}</div>
+                                <Link className={spotify || youtube || pop ? 'hideGame' : 'title'} to="/"><button className="button1" type="button">{texts[language].startRound3Instagram}</button></Link>
+                            </div>
+
+
+                            <div className="home-play-buttons">
+
+
+                                <div>
+                                    {this.state.accessToken ?
+                                        <button type="button" className={this.props.spotifyButton} onClick={this.startSpotify}>
+                                            <i class="fab fa-spotify"></i>
+                                            {texts[language].spotifyPlayWithButton}
+                                        </button>
+                                        :
+                                        <button className={this.props.tokenButton} onClick={this.getToken}>
+                                            <i class="fab fa-spotify"></i>
+                                            {texts[language].key}
+                                        </button>
+                                    }
+                                </div>
+
+                                <button type="button" className={this.props.youtubeButton} onClick={this.startYoutube}>
+                                    <i class="fab fa-youtube"></i>
+                                    {texts[language].youtubePlayWithButton}
+                                </button>
+                                <button type="button" className={this.props.instagramButton} onClick={this.startInsta}>
+                                    <i class="fab fa-instagram"></i>
+                                    {texts[language].instagramPlayWithButton}
+                                </button>
+                                <Link className={this.props.homeButton} to="/"><img className={"home-btn-image"} src={homebtn} />
+                                    {texts[language].home}</Link>
+                            </div>
+
+
+                        </div>
+                    </>
+                )}
+            </MyContext.Consumer>
         );
     }
 }
