3cd5c576e372734b07f704428d3897323599b723
Fefe Lemaire <fede.lemaire@gmail.com>
2020-08-19 15:33:09 -0300

    primeros arreglos de disenho de vouchers

--- src/Components/Register/User/UserProfile/UserProfile.js
+++ src/Components/Register/User/UserProfile/UserProfile.js
@@ -1,55 +1,55 @@
 import React, { Fragment, useState, useContext } from 'react';
 import { MyContext } from '../../../../context/MyProvider';
 import './UserProfile.css';
-import  profile  from '../../../../Pictures/user.png'
+import profile from '../../../../Pictures/user.png'
 import texts from '../../../../texts.json';
 import Api from '../../../../Api/index';
 import { Link } from 'react-router-dom';
 
 
-const UserPofile = ({language}) => {
+const UserPofile = ({ language }) => {
 
     const { state, logUserIntoContext } = React.useContext(MyContext);
 
 
-    
-   // const [username, setUsername] = useState('');
+
+    // const [username, setUsername] = useState('');
     const [phone, setPhone] = useState('');
     const [region, setRegion] = useState('');
     const [city, setCity] = useState('');
     //const [email, setEmail] = useState('');
     const [editing, setEdit] = useState(false);
-    const { state: {accessToken, user}} = React.useContext(MyContext);
+    const { state: { accessToken, user } } = React.useContext(MyContext);
 
-    
-   
-        const submitUpdate =  (e) =>{
 
-            e.preventDefault();                    
-            Api.update({
 
-                'userid': state.user,
-                "phone": phone,
-                "city": city,
-                "state": region,         
-            })
+    const submitUpdate = (e) => {
+
+        e.preventDefault();
+        Api.update({
+
+            'userid': state.user,
+            "phone": phone,
+            "city": city,
+            "state": region,
+        })
             .then((resp) => {
-            console.log(resp)                    
-            Api.getPoints(resp.data.id)
-            .then((resp2)=>{
-                let user = {...resp['data'], ...resp2['data']};
-                logUserIntoContext(user);
-            })   
-            },(err)=>{
+                console.log(resp)
+                Api.getPoints(resp.data.id)
+                    .then((resp2) => {
+                        let user = { ...resp['data'], ...resp2['data'] };
+                        logUserIntoContext(user);
+                    })
+            }, (err) => {
 
-        });
+            });
     };
 
     // const postChange = (e) => {
 
     //     e.preventDefault();
     //     console.log(username, phone, region);
-        
+
     // }
 
     return (
@@ -59,20 +59,20 @@ const UserPofile = ({language}) => {
                     <div className='profile-content'>
                         <hr />
                         <div className='myprofile-title'>
-                        <img src={context.state.picture || profile} alt="profile icon" className= 'profile-icon-user'/>                    
-                       
-                        <h1>{context.state.username}</h1>    
-                                                                         
+                            <img src={context.state.picture || profile} alt="profile icon" className='profile-icon-user' />
+
+                            <h1>{context.state.username}</h1>
+
                         </div>
                         <a onClick={() => { context.clearUser(); console.log('clicked'); }}>Salir</a>
                         <hr />
-                        
-                          {editing
 
-                        ?
+                        {editing
+
+                            ?
                             <div>
                                 <form onSubmit={submitUpdate}>
-                                {/* <input
+                                    {/* <input
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                     name="mail"
@@ -82,7 +82,7 @@ const UserPofile = ({language}) => {
                                     id="user_email"
                                 /> */}
 
-                                {/* <input
+                                    {/* <input
                                     value={username}
                                     onChange={(e) => setUsername(e.target.value)}
                                     name="username"
@@ -92,75 +92,75 @@ const UserPofile = ({language}) => {
                                     id="user_name"
                                 /> */}
 
-                                <input
-                                    value={phone}
-                                    onChange={(e) => setPhone(e.target.value)}
-                                    name="phone"
-                                    placeholder={texts[language].phone}
-                                    type="text"
-                                    className="input"
-                                    id="phone_number"
-                                />
+                                    <input
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                        name="phone"
+                                        placeholder={texts[language].phone}
+                                        type="text"
+                                        className="input"
+                                        id="phone_number"
+                                    />
 
-                                <input
-                                    value={city}
-                                    onChange={(e) => setCity(e.target.value)}
-                                    name="City"
-                                    placeholder={texts[language].city}
-                                    type="text"
-                                    className="input"
-                                    id="city"
-                                />
+                                    <input
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        name="City"
+                                        placeholder={texts[language].city}
+                                        type="text"
+                                        className="input"
+                                        id="city"
+                                    />
 
-                                <input
-                                    value={region}
-                                    onChange={(e) => setRegion(e.target.value)}
-                                    name="region"
-                                    placeholder={texts[language].state}
-                                    type="text"
-                                    className="input"
-                                    id="region"
-                                />
-                                <button className="safeBtn" type="submit">Submit</button>
-                                 </form>
+                                    <input
+                                        value={region}
+                                        onChange={(e) => setRegion(e.target.value)}
+                                        name="region"
+                                        placeholder={texts[language].state}
+                                        type="text"
+                                        className="input"
+                                        id="region"
+                                    />
+                                    <button className="safeBtn" type="submit">Submit</button>
+                                </form>
                             </div>
-                        :
+                            :
                             <div>
-                                    <h6> {texts[language].mail}:
+                                <h6> {texts[language].mail}:
                                     <p className="myprofile-h6">{context.state.email}</p>
-                                    </h6>
+                                </h6>
 
-                                    <h6>
-                                     {texts[language].username}:
+                                <h6>
+                                    {texts[language].username}:
                                     <p className="myprofile-h6">{context.state.username}</p>
-                                    </h6>
+                                </h6>
 
-                                    <h6>
-                                        {texts[language].phone}:
+                                <h6>
+                                    {texts[language].phone}:
                                         <p className="myprofile-h6">{context.state.phone}</p>
-                                    </h6>
+                                </h6>
 
-                                    <h6>
-                                        {texts[language].city}:
+                                <h6>
+                                    {texts[language].city}:
                                         <p className="myprofile-h6">{context.state.city}</p>
-                                    </h6>
+                                </h6>
 
-                                    <h6>
-                                        {texts[language].state}:
+                                <h6>
+                                    {texts[language].state}:
                                         <p className="myprofile-h6">{context.state.state}</p>
-                                     </h6>
-                                    </div>                      
-                        }           
+                                </h6>
+                            </div>
+                        }
 
                         <h6>
                             {texts[language].pointsComplete}:
                             <p className="myprofile-h6">{context.state.updateTotalPoints}</p>
                         </h6>
                         <h6>
-                             Voucher History:
+                            Voucher History:
                              {context.state.usedVouchers &&
-                                context.state.usedVouchers.map((voucher)=> 
-                                <p className="myprofile-h6">{voucher}</p>)}
+                                context.state.usedVouchers.map((voucher) =>
+                                    <p className="myprofile-h6">{voucher}</p>)}
                         </h6>
 
                         <h4 className="myprofile-h4">
@@ -168,17 +168,17 @@ const UserPofile = ({language}) => {
                         </h4>
 
                         <hr />
-                       <Link to="vouchers"> <button>Get Voucher</button></Link>
+                        <Link className='voucherBtn' to="vouchers"> <button className="voucherBtn">Get Voucher</button></Link>
                         <button className="myprofile-btn" onClick={() => setEdit(true)}>{texts[language].completeProfile}</button>
                         {editing
-                        ?
-                        <button className="cancelBtn" onClick={() => setEdit(false)}>{texts[language].cancel}</button>
-                        :
-                        <p></p>
+                            ?
+                            <button className="cancelBtn" onClick={() => setEdit(false)}>{texts[language].cancel}</button>
+                            :
+                            <p></p>
                         }
-                     
-                        
-                     
+
+
+
                     </div>
                 )}
             </MyContext.Consumer>
--- src/Components/Voucher/VoucherPage/Voucher.js
+++ src/Components/Voucher/VoucherPage/Voucher.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Vouchers from '../Vouchers.json';
 import VoucherHeader from '../Header/VoucherHeader';
 import VoucherCards from '../VoucherCards/VoucherCards';
@@ -6,66 +6,68 @@ import { MyContext } from '../../../context/MyProvider';
 import './Voucher.css';
 
 
-    const Voucher = () => {
+const Voucher = () => {
 
-        const [vouchers, setVouchers] = useState([]);
-        const [chosenVoucher, setChosenVoucher] = useState([]);
-        const [choseVoucher, setToChosen] = useState(false);
-        // const { usedVouchers } = React.useContext(MyContext);
-        const { updateVoucherHistory } = React.useContext(MyContext);
-        
-
-            
-        useEffect( () => {
-            setVouchers(Vouchers['vouchers']);
-          }, []); 
+    const [vouchers, setVouchers] = useState([]);
+    const [chosenVoucher, setChosenVoucher] = useState([]);
+    const [choseVoucher, setToChosen] = useState(false);
+    // const { usedVouchers } = React.useContext(MyContext);
+    const { updateVoucherHistory } = React.useContext(MyContext);
 
 
-        const selectVoucher = (event) => {        
-            const voucherIndex = event.target.id;
-            const selectedVoucher = vouchers[voucherIndex];
-            let myVoucher = [...chosenVoucher];
-             myVoucher.push(selectedVoucher);
-             setChosenVoucher(myVoucher);
-             console.log(myVoucher);
-             setToChosen(true);         
-        }; 
 
-       const pushToVoucherHistory = () => {          
-            updateVoucherHistory(chosenVoucher[0].title);               
-        }
-         
-    return(
- 
-    <React.Fragment> 
-        <VoucherHeader/>
+    useEffect(() => {
+        setVouchers(Vouchers['vouchers']);
+    }, []);
+
+
+    const selectVoucher = (event) => {
+        const voucherIndex = event.target.id;
+        const selectedVoucher = vouchers[voucherIndex];
+        let myVoucher = [...chosenVoucher];
+        myVoucher.push(selectedVoucher);
+        setChosenVoucher(myVoucher);
+        console.log(myVoucher);
+        setToChosen(true);
+    };
+
+    const pushToVoucherHistory = () => {
+        updateVoucherHistory(chosenVoucher[0].title);
+    }
+
+    return (
+
+        <React.Fragment>
+
             <div className="wrapper">
-                <div>          
+                <VoucherHeader />
+                <div>
                     {
-                    vouchers.map((vouch, index) => 
-                <VoucherCards       key={index} 
-                                    id={index} 
-                                    imageClass={'voucherImage'}
-                                    image={vouch.image} 
-                                    title={vouch.title} 
-                                    text={vouch.text}                                        
-                                    action={selectVoucher} 
-                                    buttonPrint={vouch.button}/>)               
-                    }           
-                </div>                   
-                    <div >                      
-                        {chosenVoucher.map((voucher, index) => 
+                        vouchers.map((vouch, index) =>
+                            <VoucherCards key={index}
+                                id={index}
+                                imageClass={'voucherImage'}
+                                image={vouch.image}
+                                title={vouch.title}
+                                text={vouch.text}
+                                action={selectVoucher}
+                                buttonPrint={vouch.button} />)
+                    }
+                </div>
+                <div >
+                    {chosenVoucher.map((voucher, index) =>
                         <section className="popUpWindow" >
                             <h2 key={index} >{voucher.title} ({voucher.quantity})</h2>
                             <p>{voucher.text}</p>
                             <img src={voucher.image}></img>
                             <button onClick={pushToVoucherHistory}>{voucher.button}</button>
                         </section>)
-                        }                  
-                    </div> 
+                    }
+                </div>
             </div>
-    </React.Fragment> 
-                  
-)};
+        </React.Fragment>
+
+    )
+};
 
 export default Voucher;
