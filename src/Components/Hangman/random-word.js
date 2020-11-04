722ad843f9ada19197fa8cb102039b2315a191d7
Sandra <alecsandra.serediuc@gmail.com>
2020-11-04 14:16:29 +0100

    Test words added

--- src/Components/Hangman/random-word.js
+++ src/Components/Hangman/random-word.js
@@ -1,8 +1,10 @@
 //import words from 'an-array-of-english-words';
-import { HelpJuanWords } from './HelpJuanWords';
+// import { HelpJuanWords } from './HelpJuanWords';
 
 export default (language) => {
-  const words = HelpJuanWords[language].words
+  // const words = HelpJuanWords[language].words
+  const words = ['hola', 'chau','mari','chichi']
+
   const wordIndex = Math.floor(Math.random() * words.length);
 
   return words[wordIndex];
